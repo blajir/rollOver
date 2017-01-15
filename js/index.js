8a1207dd1c4a456bf5f39d7589d2a783be850616
波多野潤一 <hata.junichi@gmail.com>
2017-01-15 17:21:54 +0900

    複数画像に対応

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
 /*global jQuery*/
-
 // JavaScript Document
 // UTF-8
-
 (function ($) {
   'use strict';
   $.fn.rollOver = function (params) {
-
     // デフォルト値
     var defs = {
       duration: 800,
@@ -16,62 +13,56 @@
       easing: 'linear',
       opacity: false
     };
-
-    // 渡されたオプションとデフォルト値をマージ
-    var config = $.extend({}, defs, params);
-
-    // 要素を退避
-    var element = this;
-
-    var src = element.attr('src');
-    // w+$でマッチした拡張子を取得
-    // $&でマッチした拡張子を代入
-    var src_o = src.replace(/\.\w+$/, config.suffix_replace + '$&');
-
-    if (config.fade) {
-      // 要素をマウスオンした時のイベント
-      element.on({
-        'mouseenter.rollover': function () {
-          $(this).animate({
-            'opacity': '.6'
-          }, {
-            'duration': config.duration,
-            'complete': function () {
-              $(this).animate({
-                'opacity': '1'
-              }, config.duration, config.easing);
-              this.src = src_o;
-            }
-          });
-        },
-        'mouseleave.rollover': function () {
-          this.src = src;
-        }
-      });
-    } else if (config.opacity) {
-      element.on({
-        'mouseenter.rollover': function () {
-          $(this).css('opacity', config.opacity);
-        },
-        'mouseleave.rollover': function () {
-          $(this).css('opacity', 1);
-        }
-      })
-    } else {
-      element.on({
-        'mouseenter.rollover': function () {
-          this.src = src_o;
-        },
-        'mouseleave.rollover': function () {
-          this.src = src;
-        }
-      });
-    }
-
-
-
-    // 要素を返す
-    return this;
-
+    return this.each(function(){
+      // 渡されたオプションとデフォルト値をマージ
+      var config = $.extend({}, defs, params);
+      // 要素を退避
+      var element = this;
+      var src = $(this).attr('src');
+      // w+$でマッチした拡張子を取得
+      // $&でマッチした拡張子を代入
+      var src_o = src.replace(/\.\w+$/, config.suffix_replace + '$&');
+      if (config.fade) {
+        // 要素をマウスオンした時のイベント
+        $(this).on({
+          'mouseenter.rollover': function () {
+            $(this).animate({
+              'opacity': '.6'
+            }, {
+              'duration': config.duration,
+              'complete': function () {
+                $(this).animate({
+                  'opacity': '1'
+                }, config.duration, config.easing);
+                this.src = src_o;
+              }
+            });
+          },
+          'mouseleave.rollover': function () {
+            this.src = src;
+          }
+        });
+      } else if (config.opacity) {
+        $(this).on({
+          'mouseenter.rollover': function () {
+            $(this).css('opacity', config.opacity);
+          },
+          'mouseleave.rollover': function () {
+            $(this).css('opacity', 1);
+          }
+        })
+      } else {
+        $(this).on({
+          'mouseenter.rollover': function () {
+            this.src = src_o;
+          },
+          'mouseleave.rollover': function () {
+            this.src = src;
+          }
+        });
+      }
+      // 要素を返す
+      return this;
+    });
   };
 }(jQuery));
